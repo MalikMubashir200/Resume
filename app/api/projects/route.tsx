73619da5702447a73238423dfaf1cc50dbc825ea
Malik Mubashir <m.mubashir.dev@gmail.com>
2025-07-10 11:58:45 -0700

    Create Project Api

--- app/api/projects/route.tsx
+++ app/api/projects/route.tsx
@@ -1,7 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client"
 import z from "zod";
+
 const prisma = new PrismaClient();
+
+const createProjectSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().min(1),
+  liveDemoUrl: z.string().min(1),
+  category: z.string().min(1)
+})
 
 
 export async function GET() {
@@ -9,7 +17,29 @@ export async function GET() {
    const projects = await prisma.project.findMany();
    return NextResponse.json(projects);
  } catch (err) {
-   return NextResponse.json({err:"Server side err",})
- }
+  return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 };
 
+export async function POST(req:NextRequest) {
+  try {
+    const body = await req.json()
+    const parsed = createProjectSchema.safeParse(body);
+
+     if (!parsed.success) {
+      return NextResponse.json({ error: parsed.error }, { status: 400 });
+    }
+
+    const { title,description,liveDemoUrl,category } =  parsed.data;
+    
+    const newProject = await prisma.project.create({
+      data:{title,description,liveDemoUrl,category}
+    })
+
+    return NextResponse.json(newProject,{status:201})
+    
+  } catch (err) {
+     console.error(err);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+};
