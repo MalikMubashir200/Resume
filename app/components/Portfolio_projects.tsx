6ddef946b18dd974a1a4ff66a4dfe8e892cfdec7
Malik Mubashir <m.mubashir.dev@gmail.com>
2025-07-28 04:28:25 -0700

    Sidebar err solve

--- app/components/Portfolio_projects.tsx
+++ app/components/Portfolio_projects.tsx
@@ -21,7 +21,7 @@ type PortfolioProjectsProps = {
 
 export default function PortfolioProjects({ projects, titleP1, titleP2, subtitle }: PortfolioProjectsProps) {
   return (
-    <section className="py-16 md:py-20 bg-white">
+    <section id='work' className="py-16 md:py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       
           <div className="text-center mb-12 md:mb-16 max-w-4xl mx-auto">
